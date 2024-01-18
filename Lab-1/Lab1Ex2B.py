f3db6a373b143bbdc8e162d78b127a6afd058925
Sebastian-UofA <sperez2@ualberta.ca>
2024-01-17 18:02:40 -0700

    almost done lab 1

---
+++ Lab-1/Lab1Ex2B.py
@@ -0,0 +1,31 @@
+# Read the earthquake data from the file
+with open('earthquake.txt', 'r') as file:
+    earthquake_data = file.readlines()
+
+# Create a dictionary to store the earthquake data by region
+earthquake_dict = {}
+
+# Process each line of the earthquake data
+for line in earthquake_data:
+    # Split the line into individual data elements
+    elements = line.split()
+
+    # Extract the region, magnitude, and date from the elements
+    region = elements[-1]
+    magnitude = elements[0]
+    date = elements[1]
+
+    # Check if the region already exists in the dictionary
+    if region in earthquake_dict:
+        earthquake_dict[region].append([date, magnitude])
+    else:
+        earthquake_dict[region] = [[date, magnitude]]
+
+# Write the earthquake data to the output file
+# Write the earthquake data to the output file
+with open('earthquakefmt.txt', 'w') as file:
+    for region, data in earthquake_dict.items():
+        file.write(f'[{region.upper()}, ')
+        for item in data:
+            file.write(f'[{item[0]}, {item[1]}], ')
+        file.write(']\n')
