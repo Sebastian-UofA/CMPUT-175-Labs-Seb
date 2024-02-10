a676c4e3e23ff8fa85c9e06f175f2fa2364bc5a8
Sebastian-UofA <sperez2@ualberta.ca>
2024-02-09 17:00:48 -0700

    lab4 handin seb

--- lab4_browser.py
+++ lab4_browser.py
@@ -8,59 +8,58 @@
 
 def getAction():
     '''
-    Write docstring to describe function
+    Asks the user for an action input and validates the input. Otherwise keeps giving error message until a valid input is entered.
     Inputs: None
     Returns: Valid character entered by the user (str)
     '''
-    # TO DO: delete pass and write your code here
     
     allowed_action =  ['=', '<', '>', 'q']
-    while True:
-        user_input = input('Enter = to go to a new site, < to go back, > to go forward, or q to quit: ')
-        if user_input in allowed_action:
-            return user_input
-        else:
-            user_input = input('Invalid input. Please enter =, <, >, or q: ')
 
+    user_input = input('Enter = to enter a URL, < to go back, > to go forward, or q to quit: ').strip()
+    while user_input not in allowed_action:
+        user_input = input('Invalid entry. Please enter =, <, >, or q: ').strip()
+    return user_input
 
 def goToNewSite(current, pages):
     '''
-    Write docstring to describe function
-    Inputs: ?
-    Returns: ?
-    '''   
-    # TO DO: delete pass and write your code here
-    new_site = input("URL: ")
-    pages = pages[:current + 1]  # Remove forward history
-    pages.append(new_site)  # Add new site to the end
-    return len(pages) - 1  # Return the index of the new site
+    Asks the user to enter a new website, adds this website to the list of pages and the current index.
+    Inputs: current (int) - the index of the current site, pages (list) - the list of websites
+    Returns: current (int) - the updated index of the current site
+    '''
+    newSite = input('URL: ') # Ask the user to enter a new website
+    # Append the new website to the list of pages
+    pages.append(newSite)
+    # Update the current index to point to the newly added website
+    current = len(pages) - 1  # index of the last element in the list = CURRENT 
+    return current
     
 def goBack(current, pages):
     '''
-    Write docstring to describe function
-    Inputs: ?
-    Returns: ?
-    '''    
-    # TO DO: delete pass and write your code here
-    if current == 0:
-        print("Cannot go back.")
-        return current
+    Goes back to the previous webpage if there are any webpages stored in websites list behind the current index.
+    Inputs: current (int) - the index of the current site, pages (list) - the list of websites
+    Returns: current (int) - the updated index of the current site
+    '''
+    if current > 0:
+        # If the current index is greater than 0, return the previous index
+        return current -1
     else:
-        return current - 1
-
+        # Esle, display an error message
+        print('Cannot go back.')
+    return current
 
 def goForward(current, pages):
     '''
-    Write docstring to describe function
-    Inputs: ?
-    Returns: ?
-    '''    
-    # TO DO: delete pass and write your code here
-    if current == len(pages) - 1:
-        print("Cannot go forward.")
-        return current
-    else:
+    Goes forward to the next webpage if there are any webpages stored ahead of the current index in the websites list.
+    Inputs: current (int) - the index of the current site, pages (list) - the list of websites
+    Returns: current (int) - the updated index of the current site
+    '''
+    if current < len(pages) - 1: 
+        # if current less than the last index, return the next index forward
         return current + 1
+    else:
+        # Else, display an error message
+        print('Cannot go forward.')
+    return current
 
 
 def main():
